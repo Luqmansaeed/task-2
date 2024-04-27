098939a04d7d6774bfc1e362ee6a21c18d63a90d
luqman <luqmansaeed001@gmail.com>
2024-04-27 23:12:20 +0800

    if-else

---
+++ script.js
@@ -0,0 +1,18 @@
+function detailMarks(){
+
+    let = +document.querySelector("#english").value;
+    let = +document.querySelector("#urdu").value;
+    let = +document.querySelector("#islamiyat").value;
+    let = +document.querySelector("#maths").value;
+    let = +document.querySelector("#computer").value;
+    let = +document.querySelector("#bio").value;
+    let = +document.querySelector("#chemistry").value;
+    let = +document.querySelector("#physics").value;
+    let = +document.querySelector("#gernal").value;
+    let = +document.querySelector("#statistics").value;
+    
+    let totalmarks =english + urdu + maths + bio + chemistry + physics + islamiyat + gernal + computer + statistics ;
+    let percentage = totalmarks * 100
+    
+    if()
+    }
