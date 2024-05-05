d3fed1b0b4f4408299947551538e0e7e2953d341
luqman <luqmansaeed001@gmail.com>
2024-05-06 00:08:49 +0500

    completed

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 function detailMarks(){
 
-    let = +document.querySelector("#english").value;
-    let = +document.querySelector("#urdu").value;
-    let = +document.querySelector("#islamiyat").value;
-    let = +document.querySelector("#maths").value;
-    let = +document.querySelector("#computer").value;
-    let = +document.querySelector("#bio").value;
-    let = +document.querySelector("#chemistry").value;
-    let = +document.querySelector("#physics").value;
-    let = +document.querySelector("#gernal").value;
-    let = +document.querySelector("#statistics").value;
+    let english = parseInt(document.querySelector("#english").value);
+    let urdu = parseInt(document.querySelector("#urdu").value);
+    let maths = parseInt(document.querySelector("#maths").value);
+    let islamiyat = parseInt(document.querySelector("#islamiyat").value);
+    let bio = parseInt(document.querySelector("#bio").value);
+    let chemistry = parseInt(document.querySelector("#chemistry").value);
+    let physics = parseInt(document.querySelector("#physics").value);
+    let gernal = parseInt(document.querySelector("#gernal").value);
+    let computer = parseInt(document.querySelector("#computer").value);
+    let statistics = parseInt(document.querySelector("#statistics").value);
     
-    let totalmarks =english + urdu + maths + bio + chemistry + physics + islamiyat + gernal + computer + statistics ;
-    let percentage = totalmarks * 100
+    let totals = parseInt(document.querySelector("#totals").value);
     
-    if()
+    let obtainmark = english + urdu + maths + bio + chemistry + physics + islamiyat + gernal + computer + statistics ;
+    // let obtainmark = english + urdu + maths + bio + chemistry + physics + islamiyat + gernal + computer + statistics ;
+    // let obtainmarks = `${english}${urdu}${maths}${bio}${chemistry}${physics}${islamiyat}${gernal}${computer}${statistics}`;
+    let totalmarks = totals;
+    let percentage = (obtainmark / totalmarks) * 100;
+    
+    if(percentage >= 80){
+        document.querySelector("#obtainmarks").innerHTML = `Obtain Marks = ${obtainmark} `; 
+        document.querySelector("#totals").innerHTML = `Totals marks = ${totalmarks} `; 
+        document.querySelector("#percent").innerHTML = `Percentage = ${percentage}%  `; 
+        document.querySelector("#grade").innerHTML = `Grade A1 `; 
+
+    }else if(percentage >= 70){
+        document.querySelector("#obtainmarks").innerHTML = `Obtain Marks = ${obtainmark}  `; 
+        document.querySelector("#totals").innerHTML = `Totals marks = ${totalmarks}  `; 
+        document.querySelector("#percent").innerHTML = `Percentage = ${percentage}% `; 
+        document.querySelector("#grade").innerHTML = `Grade A `; 
+    }else if(percentage >= 60){
+        document.querySelector("#obtainmarks").innerHTML = `Obtain Marks = ${obtainmark} `; 
+        document.querySelector("#totals").innerHTML = `Totals marks = ${totalmarks} `; 
+        document.querySelector("#percent").innerHTML = `Percentage = ${percentage}% `; 
+        document.querySelector("#grade").innerHTML = `You have Got Grade B `; 
+    }else if(percentage >= 50){
+        document.querySelector("#obtainmarks").innerHTML = `Obtain Marks = ${obtainmark} `; 
+        document.querySelector("#totals").innerHTML = `Totals marks = ${totalmarks} `; 
+        document.querySelector("#percent").innerHTML = `Percentage = ${percentage}% `; 
+        document.querySelector("#grade").innerHTML = `You have Got Grade C `; 
+    }else if(percentage >= 40){
+        document.querySelector("#obtainmarks").innerHTML = `Obtain Marks = ${obtainmark} `; 
+        document.querySelector("#totals").innerHTML = `Totals marks = ${totalmarks} `; 
+        document.querySelector("#percent").innerHTML = `Percentage = ${percentage}% `; 
+        document.querySelector("#grade").innerHTML = `You have Got Grade D `; 
+    }else{
+        document.querySelector("#obtainmarks").innerHTML = `Allah  `; 
+        document.querySelector("#totals").innerHTML = `Allah `; 
+        document.querySelector("#percent").innerHTML = `khair `;
+        document.querySelector("#grade").innerHTML = `Code problem `; 
     }
+}
